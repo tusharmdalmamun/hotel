620b4be28d425015d0007a1897a2af88dd13140e
tusharmdalmamun <tusharmdalmamun@gmail.com>
2024-10-06 20:06:17 +0600

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,70 @@
+<div id="messenger"><span class="badge text-bg-primary">Live Chat</span><img width="60" height="60" src="https://img.icons8.com/fluency/60/speech-bubble-with-dots--v2.png" alt="speech-bubble-with-dots--v2"/></div>
+<div id="footer" class="container-fluid margin bg-dark text-light pt-5 ps-5 pe-5">
+    <div class="row">
+        <div class="col-sm-3 border-down">
+            <h5 class="text-light mt-5">Important Links</h5>
+            <a href="https://mdalmamuntushar.000webhostapp.com/hotel">Home</a>
+            <br>
+            <a href="about">About</a>
+            <br>
+            <a href="services">Services</a>
+            <br>
+            <a href="rooms">Rooms</a>
+            <br>
+            <a href="contact">Contact</a>
+        </div>
+        <div class="col-sm-3 border-down">
+            <h5 class="mt-5">Quick Links</h5>
+            <a href="javascript:void(0)">Terms & Conditions</a>
+            <br>
+            <a href="javascript:void(0)">Privacy Policy</a>
+            <br>
+            <a href="javascript:void(0)">Career</a>
+            <br>
+            <a href="javascript:void(0)">Branch</a>
+        </div>
+        <div class="col-sm-3">
+            <h5 class="mt-5">Get In Touch</h5>
+            <p>Highway Road, 78/23a lorem Street, ttld, USA</p>
+            <a href="javascript:void(0)"><i class="bi bi-facebook git"></i></a>
+            <a href="javascript:void(0)"><i class="bi bi-youtube git"></i></a>
+            <a href="javascript:void(0)"><i class="bi bi-pinterest git"></i></a>
+            <a href="javascript:void(0)"><i class="bi bi-instagram git"></i></a>
+        </div>
+        <div class="col-sm-3">
+            <h5 class="mt-5">Payment Method</h5>
+            <img width="30" height="30" src="https://img.icons8.com/color/48/paypal.png" alt="paypal"/>
+            <img width="30" height="30" src="https://img.icons8.com/color/48/payoneer.png" alt="payoneer"/>
+            <img width="30" height="30" src="https://img.icons8.com/color/48/visa.png" alt="visa"/>
+            <img width="30" height="30" src="https://img.icons8.com/color/48/american-express-squared.png" alt="american-express-squared"/>
+            <img width="30" height="30" src="https://img.icons8.com/color/48/mastercard.png" alt="mastercard"/>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-12">
+            <center>
+                <p>Copyright &copy; Hotel <?php echo date("Y"); ?> | All Rights Reserved | Icons By <a href="https://icons8.com/" target="_blank"><u>Icons8</u></a></p>
+            </center>
+        </div>
+    </div>
+</div>
+<button onclick="topFunction()" id="myBtn"><i class="bi bi-arrow-up"></i></button>
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Jquery -->
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<!-- AOS -->
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<!--Custom JS-->
+<script src="./js/function.js"></script>
+<script>
+    AOS.init({
+        offset:200,
+        delay:50,
+        duration:1000,
+        once:true,
+    });
+  </script>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,49 @@
+    <link rel="icon" type="image/x-icon" href="./src/favicon.png">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- AOS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg bg-transparent navbar-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="https://mdalmamuntushar.000webhostapp.com/hotel"><img style="margin-bottom:10px;" width="30" height="30" src="https://img.icons8.com/papercut/30/star.png" alt="star"/> Hotel <img style="margin-bottom:10px;" width="30" height="30" src="https://img.icons8.com/papercut/30/star.png" alt="star"/></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 myMenu">
+        <li class="nav-item">
+          <a class="nav-link" href="https://mdalmamuntushar.000webhostapp.com/hotel"><i class="bi bi-house-fill"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="rooms">Rooms</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="services">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0);">Sign Up</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0);">Sign In</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)"><i class="bi bi-telephone-fill"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)"><i class="bi bi-envelope-fill"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
